e52f74b3eb156d563aacfe4c6ea06542a0207ee4
MAHESH KURREY <maheshwarkurrey80@gmail.com>
2026-08-25 08:59:30 +0000

    configation commit doe

---
+++ basics/Datatype.js
@@ -0,0 +1,10 @@
+"use strict";// tret as everything is new
+
+// alert("hii")//we are using node not browser
+
+
+let name="mahesh"; // string
+let age=19; // int data type
+let isloggin=false; //boolean data type
+let yes=null;
+let clas=undefined;
